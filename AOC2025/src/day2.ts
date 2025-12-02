472ddf210c89c8e27994b94a41dee18d06c70658
JavanW-W <javan.whitney-warner@numat.com>
2025-12-02 10:41:12 -0600

    :star: Day 2 Part 1

---
+++ AOC2025/src/day2.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+
+// Part 1
+export function day2_part1(file_path: string): number {
+    let input = fs.readFileSync(file_path, 'utf8');
+    let ranges: string[] = input.split(",");
+    let invalid_ids: number[] = []
+    ranges.forEach((range) => {
+        let id: number = parseInt(range.split("-")[0])
+        let end: number = parseInt(range.split("-")[1])
+        while(id < end + 1) {
+            // check if it's invalid
+            let len: number = id.toString().length
+            if (len%2 == 0) {
+                if (id.toString().substring(0, len/2) == id.toString().slice(len/2)) {
+                    invalid_ids.push(id)
+                }
+            }
+            id += 1
+        }
+    })
+
+    let sum = invalid_ids.reduce(
+        (accumulator, current_id) => accumulator + current_id
+    )
+    return sum
+}
+
+console.log('Part 1:', day2_part1('./inputs/day2.txt'));
---
+++ AOC2025/tests/day2.test.ts
@@ -0,0 +1,9 @@
+// Part 1
+
+import { day2_part1 } from '../src/day2';
+
+describe('testing AoC 2025 Day 2 Part 1', () => {
+  test('test case should give 1227775554', () => {
+    expect(day2_part1('./inputs/test/day2.txt')).toBe(1227775554);
+  });
+});
